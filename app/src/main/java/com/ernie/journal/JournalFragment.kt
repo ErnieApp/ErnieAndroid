0f77d800e036e8580917c97df1df023f5a6ed6ca
SadaqueKhan <sadaque.k@gmail.com>
2019-07-27 20:35:15 +0100

    Add framelayout to switch out fragments in Journal
    Signed-off-by: SadaqueKhan <sadaque.k@gmail.com>

--- app/src/main/java/com/ernie/journal/JournalFragment.kt
+++ app/src/main/java/com/ernie/journal/JournalFragment.kt
@@ -4,12 +4,14 @@ package com.ernie.journal
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
-
 import android.support.v4.view.ViewPager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ernie.R
+
+private const val TAG = "Journal Class"
 
 
 class JournalFragment : Fragment() {
@@ -20,17 +22,20 @@ class JournalFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        Log.d(TAG, "onCreate: starts")
         val view: View = inflater.inflate(R.layout.fragment_journal, container, false)
 
+        addFragment()
+        Log.d(TAG, "onCreate: ends")
         return view
     }
 
     private fun addFragment() {
 
-
-        val ft = fragmentManager!!.beginTransaction()
-        ft.add(R.id.fragmentContainer, journal_list_dest)
-        ft.commit()
+        val newFragment = JournalListFragmentViewModel.newInstance("Hello", "World")
+        fragmentManager!!.beginTransaction()
+                .replace(R.id.fragmentContainer, newFragment)
+                .commit()
 
 
     }

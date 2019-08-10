32e8e6e1a09b9e8440e3b6d7d1051f3e9e8efa4a
Mert Acar <k1631444@kcl.ac.uk>
2019-08-10 19:41:00 +0100

    Identify if user signing in with google is new

--- app/src/main/java/com/ernie/LoginActivity.kt
+++ app/src/main/java/com/ernie/LoginActivity.kt
@@ -2,17 +2,19 @@ package com.ernie
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.util.Patterns
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.firebase.ui.auth.AuthMethodPickerLayout
-import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -22,48 +24,68 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setupBtnLoginListener()
+        setupBtnGoogleLoginListener()
 
+        forgotPasswordTextView.setOnClickListener {
+            val userEmail = fieldEmail.text.toString()
+            if (isValidEmail(userEmail)) {
+                fireAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "Email sent", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, "Error", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun isValidEmail(target: CharSequence): Boolean {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
+    }
+
+    private fun guideUserHome() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
+        finish()
+    }
+
+    private fun setupBtnLoginListener() {
         btnLogin.setOnClickListener {
-
             val userEmail = fieldEmail.text.toString()
             val userPassword = fieldPassword.text.toString()
 
-            if (userEmail.isNotBlank() && userPassword.isNotBlank()) {
+            if (isValidEmail(userEmail) && userPassword.isNotBlank()) {
                 fireAuth.signInWithEmailAndPassword(userEmail, userPassword)
                         .addOnCompleteListener {
                             // Check if user has logged in successfully
                             if (fireAuth.currentUser != null) {
-                                val intent = Intent(this, MainActivity::class.java)
-                                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
-                                finish()
+                                guideUserHome()
                             } else {
                                 invalidCredentialsTextView.visibility = View.VISIBLE
                             }
                         }
             }
         }
+    }
 
+    private fun setupBtnGoogleLoginListener() {
         btnGoogleLogin.setOnClickListener {
-
             val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                     .requestIdToken(getString(R.string.default_web_client_id))
                     .requestEmail()
                     .build()
 
-            // Configure Google Sign In Initialization
-            val googleSignInClient = GoogleSignIn.getClient(this, gso)
-
-            val signInIntent = googleSignInClient.signInIntent
-            startActivityForResult(signInIntent, RC_SIGN_IN)
-
+            val signInIntent = GoogleSignIn.getClient(this, gso).signInIntent
+            startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN)
         }
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(ApiException::class.java)
-            Log.e("JKE", "Google sign in success : " + account!!)
-
+            firebaseAuthWithGoogle(account!!)
         } catch (e: ApiException) {
             Log.e("JKE", "Google sign in failed")
             Log.w("JKE", "signInResult:failed code=" + e.statusCode)
@@ -71,65 +93,50 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d("JKE", "firebaseAuthWithGoogle:" + acct.id!!)
 
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
 
-    private fun createSignInIntent() {
+        fireAuth.fetchSignInMethodsForEmail(acct.email!!)
+                .addOnCompleteListener { task ->
+                    val isNewUser = task.result!!.signInMethods!!.isEmpty()
 
-        val customLayout: AuthMethodPickerLayout = AuthMethodPickerLayout
-                .Builder(R.layout.activity_login)
-                .setGoogleButtonId(R.id.btnGoogleLogin)
-//                .setTosAndPrivacyPolicyId(R.id.baz)
-                .build()
+                    if (isNewUser) {
+                        Log.e("TAG", "Is New User!")
 
-        // [START auth_fui_create_intent]
+                        //TODO: Collect more data before creating their account
 
-        // Choose authentication providers
+                    } else {
+                        Log.e("TAG", "Is Old User!")
 
-        val providers = arrayListOf(
-
-//                AuthUI.IdpConfig.EmailBuilder().build(),
-
-//                AuthUI.IdpConfig.PhoneBuilder().build(),
-
-                AuthUI.IdpConfig.GoogleBuilder().build())
-
-
-        // Create and launch sign-in intent
-
-        startActivityForResult(
-
-                AuthUI.getInstance()
-
-                        .createSignInIntentBuilder()
-
-                        .setAvailableProviders(providers)
-
-                        .setAuthMethodPickerLayout(customLayout)
-
-                        .setTosAndPrivacyPolicyUrls("google.com", "yahoo.co.uk")
-
-                        .build(),
-
-                RC_SIGN_IN)
-
-        // [END auth_fui_create_intent]
-
+                        fireAuth.signInWithCredential(credential)
+                                .addOnCompleteListener(this) { task ->
+                                    if (task.isSuccessful) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d("JKE", "signInWithCredential:success")
+                                        guideUserHome()
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w("JKE", "signInWithCredential:failure", task.exception)
+                                        Toast.makeText(this, "FAILED", Toast.LENGTH_LONG)
+                                    }
+                                }
+                    }
+                }
     }
-
-
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
+        if (requestCode == RC_GOOGLE_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             handleSignInResult(task)
-
         }
     }
 
     companion object {
-        private const val RC_SIGN_IN = 123
+        private const val RC_GOOGLE_SIGN_IN = 444
     }
 }

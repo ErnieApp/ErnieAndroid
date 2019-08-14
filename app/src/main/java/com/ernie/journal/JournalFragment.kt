8e0d09bcf8a5d148228e8c954b442068d63b5f75
Mert Acar <k1631444@kcl.ac.uk>
2019-08-14 10:47:28 +0100

    Replace Sadaque's hiding method to fragmentManager

--- app/src/main/java/com/ernie/journal/JournalFragment.kt
+++ app/src/main/java/com/ernie/journal/JournalFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.ernie.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_journal.*
 
 class JournalFragment : Fragment() {
@@ -21,25 +24,19 @@ class JournalFragment : Fragment() {
         journalListExpandedEntryFragment = JournalListExpandedEntryFragment()
     }
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_journal, container, false)
-
-        displayFragmentA()
-
+        setupFloatingActionButtonListener(view.findViewById(R.id.floatingActionButton))
+        showEntryList()
         return view
     }
 
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        floatingActionButton.setOnClickListener {
+    private fun setupFloatingActionButtonListener(button: FloatingActionButton) {
+        button.setOnClickListener {
             if (journalListAddEntryFragment.isVisible) {
-                displayFragmentA()
+                showEntryList()
             } else {
-                displayFragmentB()
+                showEntryAddForm()
             }
 
             if (floatingActionButton.rotation != 45f) {
@@ -50,76 +47,26 @@ class JournalFragment : Fragment() {
         }
     }
 
-    protected fun displayFragmentA() {
-        val ft = childFragmentManager.beginTransaction()
-
-        if (journalListFragment.isAdded) {
-
-            ft.show(journalListFragment)
-        } else {
-            ft.add(R.id.fragmentContainer, journalListFragment, "journalListFragment")
-        }
-        // Hide fragment
-        if (journalListAddEntryFragment.isAdded) {
-            ft.hide(journalListAddEntryFragment)
-        }
-        if (journalListExpandedEntryFragment.isAdded) {
-            ft.hide(journalListExpandedEntryFragment)
-        }
-
-        // Commit changes
-        ft.commit()
+    private fun showEntryList() {
+        replaceCurrentFragment(R.id.journalFrameContainer, journalListFragment)
     }
 
-
-    protected fun displayFragmentB() {
-        val ft = childFragmentManager.beginTransaction()
-
-        if (journalListAddEntryFragment.isAdded) {
-
-            ft.show(journalListAddEntryFragment)
-        } else {
-
-            ft.add(R.id.fragmentContainer, journalListAddEntryFragment, "journalListFragment")
-
-        }
-        // Hide fragment
-        if (journalListFragment.isAdded) {
-            ft.hide(journalListFragment)
-        }
-        if (journalListExpandedEntryFragment.isAdded) {
-            ft.hide(journalListExpandedEntryFragment)
-        }
-
-        // Commit changes
-        ft.commit()
+    private fun showEntryAddForm() {
+        replaceCurrentFragment(R.id.journalFrameContainer, journalListAddEntryFragment)
     }
 
-    fun displayFragmentC() {
-        val ft = childFragmentManager.beginTransaction()
+    private fun showExpandedEntry() {
+        replaceCurrentFragment(R.id.journalFrameContainer, journalListExpandedEntryFragment)
+    }
 
-        if (journalListExpandedEntryFragment.isAdded) {
-
-            ft.show(journalListExpandedEntryFragment)
-        } else {
-
-            ft.add(R.id.fragmentContainer, journalListExpandedEntryFragment, "journalListFragment")
-
-        }
-        // Hide fragment
-        if (journalListFragment.isAdded) {
-            ft.hide(journalListFragment)
-        }
-        if (journalListAddEntryFragment.isAdded) {
-            ft.hide(journalListAddEntryFragment)
-        }
-
-        // Commit changes
-        ft.commit()
+    private fun replaceCurrentFragment(@IdRes containerViewId: Int, fragment: Fragment) {
+        val fragmentManager: FragmentManager = activity!!.supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(containerViewId, fragment)
+        transaction.commit()
     }
 
     companion object {
         private const val TAG = "JournalFragment"
     }
-
 }

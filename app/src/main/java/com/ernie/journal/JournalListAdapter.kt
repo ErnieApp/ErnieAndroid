c6eabf1b7b9ba4a9b2f47316774b8868ffed333b
Mert Acar <k1631444@kcl.ac.uk>
2019-08-17 14:05:38 +0100

    Implement selection mode

--- app/src/main/java/com/ernie/journal/JournalListAdapter.kt
+++ app/src/main/java/com/ernie/journal/JournalListAdapter.kt
@@ -1,36 +1,78 @@
 package com.ernie.journal
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ernie.AppDatabase
 import com.ernie.R
 import com.ernie.model.Entry
 
-class JournalListAdapter(private var entryList: MutableList<Entry>, private val appDatabase: AppDatabase) : RecyclerView.Adapter<JournalListAdapter.ViewHolder>() {
+class JournalListAdapter(private var entryList: MutableList<Entry>, private val appDatabase: AppDatabase, private val journalListFragment: JournalListFragment) : RecyclerView.Adapter<JournalListAdapter.ViewHolder>() {
+    private lateinit var recyclerView: RecyclerView
+    private val selectedEntryViewHolders: MutableSet<ViewHolder> = mutableSetOf()
+    private var isSelectionMode = false
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_journal_list_entry, parent, false)
         return ViewHolder(view)
     }
 
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
+    }
+
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val entry = entryList[position]
 
+        viewHolder.positionInList = position
         viewHolder.entryDate.text = entry.date_recorded.toString()
         viewHolder.entryWorkedTime.text = entry.start_time + " - " + entry.end_time
         viewHolder.entryBreakHours.text = entry.break_duration.toString() + " minutes"
         viewHolder.entryEarned.text = "£" + entry.earned
 
         viewHolder.itemView.setOnClickListener {
-            //TODO Implement vertical swipe for delete functionality and update firestore
-            appDatabase.deleteEntry(entry)
-            entryList.removeAt(position)
-            notifyItemRemoved(position)
-            notifyItemRangeChanged(position, entryList.size)
-            notifyDataSetChanged()
+            if (isSelectionMode) {
+                if (selectedEntryViewHolders.contains(viewHolder)) {
+                    removeHighlight(viewHolder)
+                    selectedEntryViewHolders.remove(viewHolder)
+                    journalListFragment.setSelectedAllCheckBoxStatus(false)
+                } else {
+                    highlightEntry(viewHolder)
+                    selectedEntryViewHolders.add(viewHolder)
+                    if (selectedEntryViewHolders.size == entryList.size) {
+                        journalListFragment.setSelectedAllCheckBoxStatus(true)
+                    }
+                }
+                viewHolder.checkBox.isChecked = !viewHolder.checkBox.isChecked
+                journalListFragment.updateSelectionCountText(selectedEntryViewHolders.size)
+            }
+        }
+
+        viewHolder.itemView.setOnLongClickListener {
+            if (!isSelectionMode) {
+                highlightEntry(viewHolder)
+                viewHolder.itemView.findViewById<CheckBox>(R.id.checkBox).isChecked = true
+                selectedEntryViewHolders.add(viewHolder)
+                journalListFragment.updateSelectionCountText(selectedEntryViewHolders.size)
+                toggleSelectionMode()
+            } else {
+                viewHolder.itemView.callOnClick()
+            }
+            true
+        }
+
+        if (isSelectionMode) {
+            viewHolder.checkBox.visibility = View.VISIBLE
+        } else {
+            viewHolder.checkBox.visibility = View.GONE
+            viewHolder.checkBox.isChecked = false
+            journalListFragment.setSelectedAllCheckBoxStatus(false)
+            removeHighlight(viewHolder)
         }
     }
 
@@ -39,15 +81,64 @@ class JournalListAdapter(private var entryList: MutableList<Entry>, private val 
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var positionInList: Int = -1
         var entryDate: TextView = view.findViewById(R.id.entryDate)
         var entryEarned: TextView = view.findViewById(R.id.entryEarned)
         var entryBreakHours: TextView = view.findViewById(R.id.entryBreakHours)
         var entryWorkedTime: TextView = view.findViewById(R.id.entryWorkedTime)
+        var checkBox: CheckBox = view.findViewById(R.id.checkBox)
     }
 
     fun updateRecords(el: MutableList<Entry>) {
         entryList = el
         notifyDataSetChanged()
+    }
+
+    fun setSelectAllEntries(selectAll: Boolean) {
+        if (selectAll) {
+            for (i in 0 until recyclerView.childCount) {
+                val viewHolder = recyclerView.findViewHolderForAdapterPosition(i) as ViewHolder
+                viewHolder.checkBox.isChecked = true
+                highlightEntry(viewHolder)
+                selectedEntryViewHolders.add(viewHolder)
+            }
+        } else {
+            for (i in 0 until recyclerView.childCount) {
+                val viewHolder = recyclerView.findViewHolderForAdapterPosition(i) as ViewHolder
+                viewHolder.checkBox.isChecked = false
+                removeHighlight(viewHolder)
+            }
+            selectedEntryViewHolders.clear()
+        }
+        journalListFragment.updateSelectionCountText(selectedEntryViewHolders.size)
+    }
+
+    fun deleteSelectedEntries() {
+        var i = 0
+        selectedEntryViewHolders.forEach { vh ->
+            appDatabase.deleteEntry(entryList.removeAt(vh.positionInList - i))
+            i++
+        }
+        toggleSelectionMode()
+    }
+
+    private fun toggleSelectionMode() {
+        isSelectionMode = !isSelectionMode
+        if (isSelectionMode) {
+            journalListFragment.setSelectionModeContainerVisibility(View.VISIBLE)
+        } else {
+            journalListFragment.setSelectionModeContainerVisibility(View.GONE)
+            selectedEntryViewHolders.clear()
+        }
+        notifyDataSetChanged()
+    }
+
+    private fun highlightEntry(viewHolder: ViewHolder) {
+        viewHolder.itemView.setBackgroundColor(Color.rgb(230, 230, 230))
+    }
+
+    private fun removeHighlight(viewHolder: ViewHolder) {
+        viewHolder.itemView.setBackgroundColor(Color.WHITE)
     }
 
     companion object {
--- app/src/main/java/com/ernie/journal/JournalListFragment.kt
+++ app/src/main/java/com/ernie/journal/JournalListFragment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,9 +17,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
+import kotlinx.android.synthetic.main.fragment_journal_list.*
 
 private lateinit var snapShotListenerRegistration: ListenerRegistration
-
 class JournalListFragment : Fragment() {
 
     private val firestoreDB = FirebaseFirestore.getInstance()
@@ -27,8 +29,19 @@ class JournalListFragment : Fragment() {
         val view: View = inflater.inflate(R.layout.fragment_journal_list, container, false)
 
         recyclerView = view.findViewById(R.id.journalRecyclerView) as RecyclerView
-        recyclerView.adapter = JournalListAdapter(mutableListOf(), appDatabase!!)
+        recyclerView.adapter = JournalListAdapter(mutableListOf(), appDatabase!!, this)
         recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        val selectAllCheckBox = view.findViewById(R.id.selectAllCheckBox) as CheckBox
+        selectAllCheckBox.setOnClickListener {
+            (recyclerView.adapter as JournalListAdapter).setSelectAllEntries(selectAllCheckBox.isChecked)
+        }
+
+        val deleteButton = view.findViewById(R.id.deleteIcon) as ImageView
+        deleteButton.setOnClickListener {
+            (recyclerView.adapter as JournalListAdapter).deleteSelectedEntries()
+        }
+
         return view
     }
 
@@ -55,8 +68,21 @@ class JournalListFragment : Fragment() {
                 })
     }
 
+    fun setSelectionModeContainerVisibility(value: Int) {
+        selectionModeContainer.visibility = value
+    }
+
+    fun updateSelectionCountText(count: Int) {
+        selectionCount.text = count.toString() + " selected"
+    }
+
+    fun setSelectedAllCheckBoxStatus(value: Boolean) {
+        (activity!!.findViewById(R.id.selectAllCheckBox) as CheckBox).isChecked = value
+    }
+
     companion object {
         private val TAG = JournalListFragment::class.simpleName
         private val appDatabase: AppDatabase? = AppDatabase()
     }
 }
+
